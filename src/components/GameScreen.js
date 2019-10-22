06571cb7f989f0cbd88a0f785c379b1dd4c86e49
mjfalahee <mjfalahee@gmail.com>
2019-10-21 19:50:50 -0700

    Room information displaying

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -6,7 +6,10 @@ import './styles.scss'
 function GameScreen(props) {
     return (
         <div className="game-screen"> 
-            <p> This would be the game messages.</p> 
+            <p> You are currently in:</p> 
+            <h3 className="room-title">{props.roomData.title ? props.roomData.title : "Initialize your location to begin"}</h3>
+            <h5 className="room-coordinates">{props.roomData.coordinates ? props.roomData.coordinates : ""}</h5>
+            <h5> {props.roomData.description ? props.roomData.description: ''} </h5>
         </div>
         
     )
